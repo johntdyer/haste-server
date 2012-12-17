cb6fb218ea8c0d5e581c04e653f73f056f21c416
John Dyer <johntdyer@gmail.com>
2012-12-16 22:12:06 -0500

    Added basic auth

--- config.js
+++ config.js
@@ -1,16 +1,10 @@
 {
-
   "host": "0.0.0.0",
   "port": 7777,
-
   "keyLength": 10,
-
   "maxLength": 400000,
-
   "staticMaxAge": 86400,
-
   "recompressStaticAssets": true,
-
   "logging": [
     {
       "level": "verbose",
@@ -18,21 +12,16 @@
       "colorize": true
     }
   ],
-
   "keyGenerator": {
     "type": "phonetic"
   },
-
   "storage": {
-    "type": "redis",
-    "host": "0.0.0.0",
-    "port": 6379,
-    "db": 2,
-    "expire": 2592000
+  "path": "./data",
+  "type": "file"
   },
-
   "documents": {
     "about": "./about.md"
-  }
-
+  },
+"user":"voxeolabs",
+"pass": "voxeolabs"
 }
--- server.js
+++ server.js
@@ -12,6 +12,12 @@ var config = JSON.parse(fs.readFileSync('./config.js', 'utf8'));
 config.port = process.env.PORT || config.port || 7777;
 config.host = process.env.HOST || config.host || 'localhost';
 
+
+//the generic auth function
+function authorize(username, password) {
+  return config.user === username & config.pass === password;
+}
+
 // Set up the logger
 if (config.logging) {
   try {
@@ -101,6 +107,8 @@ var documentHandler = new DocumentHandler({
 
 // Set the server up with a static cache
 connect.createServer(
+  //check user & pass first before anything
+  connect.basicAuth(authorize),
   // First look for api calls
   connect.router(function(app) {
     // get raw documents - support getting with extension
